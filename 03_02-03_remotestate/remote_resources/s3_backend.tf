644dddafc8124a8136cab11f6fd77305e00516dc
srinibasswain <51942428+srinibasswain@users.noreply.github.com>
2021-09-30 16:32:00 +0530

    Update s3_backend.tf
    updating region

--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -15,14 +15,14 @@ variable "bucket_name" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 # //////////////////////////////
 # TERRAFORM USER
 # //////////////////////////////
 data "aws_iam_user" "terraform" {
-  user_name = "terraform"
+  user_name = "svc_terraform"
 }
 
 # //////////////////////////////
